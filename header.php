b6778b14d4e5d01503d95bf16fc79df84a48631a
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2020-04-21 21:11:00 +0200

    Añadido el logo a la web.

--- header.php
+++ header.php
@@ -57,15 +57,29 @@
 					<?php } ?>
 
 					<div class="header-titles">
-
-						<?php
-							// Site title or logo.
-							twentytwenty_site_logo();
-
-							// Site description.
-							// twentytwenty_site_description();
-						?>
-
+						<a class="site-title" href="<?php echo get_home_url(); ?>">
+							<svg id="three" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 306 185">
+								<defs>
+								  <style>
+								  </style>
+								</defs>
+								<title>cssjslablogo</title>
+								<rect class="cls-1" x="0.5" y="2.5" width="27" height="178" />
+								<path class="cls-2" d="M847.5,394.5v9a61.5,61.5,0,1,0,0,99v8h25v-116Zm-36.5,99A41.5,41.5,0,1,1,852.5,452,41.5,41.5,0,0,1,811,493.5Z" transform="translate(-709 -330)" />
+								<path class="cls-3" d="M847.5,394.5v9a61.5,61.5,0,1,0,0,99v8h25v-116Zm-36.5,99A41.5,41.5,0,1,1,852.5,452,41.5,41.5,0,0,1,811,493.5Z" transform="translate(-709 -330)" />
+								<path class="cls-4" d="M829.5,839.5" transform="translate(-709 -330)" />
+								<path class="cls-5" d="M982.5,456c-1.38-3.48-6.17-5.48-10-5.53-8.17-.11-10.39,8.68-19,12-13.7,5.28-27.76-9.42-30.26-6.47-2.85,3.37,9.71,29.37,29.63,29.47C970.89,485.59,985.83,464.43,982.5,456Z" transform="translate(-709 -330)" />
+								<path class="cls-6" d="M982.5,456c-1.38-3.48-6.17-5.48-10-5.53-8.17-.11-10.39,8.68-19,12-13.7,5.28-27.76-9.42-30.26-6.47-2.85,3.37,9.71,29.37,29.63,29.47C970.89,485.59,985.83,464.43,982.5,456Z" transform="translate(-709 -330)" />
+								<path class="cls-4" d="M1073.5,571.5" transform="translate(-709 -330)" />
+								<path class="cls-2" d="M972,394.49c-1.44-.46-2.89-.88-4.37-1.24s-2.74-.61-4.13-.85v19.44a41.5,41.5,0,1,1-21,0V392.4a61.26,61.26,0,0,0-26,11.11v-71h-25v178h25v-8a61.5,61.5,0,1,0,55.5-108Z" transform="translate(-709 -330)" />
+								<path class="cls-3" d="M972,394.49c-1.44-.46-2.89-.88-4.37-1.24s-2.74-.61-4.13-.85v19.44a41.5,41.5,0,1,1-21,0V392.4a61.26,61.26,0,0,0-26,11.11v-71h-25v178h25v-8a61.5,61.5,0,1,0,55.5-108Z" transform="translate(-709 -330)" />
+								<path class="cls-7" d="M722.29,399.45" transform="translate(-709 -330)" />
+								<path class="cls-7" d="M722.29,399.45" transform="translate(-709 -330)" />
+								<circle class="cls-8" cx="250.5" cy="42.5" r="8" />
+								<circle class="cls-8" cx="237" cy="20" r="5.5" />
+								<circle class="cls-8" cx="252.5" cy="4.5" r="4" />
+							  </svg>
+							</a>
 					</div><!-- .header-titles -->
 
 					<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
